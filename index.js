5f46e15d842a59f0d6bb3cdab7425a2a130e6408
Tldev-ai <tutorleapofficial@gmail.com>
2026-03-09 22:11:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8080;
